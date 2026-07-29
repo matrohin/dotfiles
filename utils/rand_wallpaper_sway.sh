13c0f169da44a97903332dabeb6016a86bc2e983
Dmitrii Matrokhin <matrokhin.d@gmail.com>
2026-07-30 00:11:48 +0200

    Reapply wallpaper on sway reload

--- utils/rand_wallpaper_sway.sh
+++ utils/rand_wallpaper_sway.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 DIR=~/Pictures/wallpapers
 WALL=$(find "$DIR" -type f | shuf -n 1)
+# Bail out rather than clear the bg and the cached path lock.sh reads
+[ -n "$WALL" ] || exit 0
 swaymsg output '*' bg "$WALL" fill
 echo "$WALL" > ~/.cache/wallpaper
