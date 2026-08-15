119579cc92a1bccc814bf6ad8577e27c8016b750
Dmitrii Matrokhin <matrokhin.d@gmail.com>
2026-08-15 02:34:19 +0200

    Revert color experiments
    This reverts commit bad8e2dc8744bdf064b82223e4bf3159e6ee9fd6.
    This reverts commit 31b05fc929434296728129b91401c8dccd20f3a3.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,13 +8,13 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 set -g fish_prompt_pwd_dir_length 3
 
 function fish_right_prompt
-  set_color b48ead
+  set_color 5e81ac
   echo (prompt_pwd)
   set_color normal
 end
 
 function fish_prompt
-  set_color d08770
+  set_color 81a1c1
   echo -n '$ '
   set_color normal
 end
